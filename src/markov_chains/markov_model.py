c270233c34614524d7a803c261d2ea981fb61a39
Manu Varma <mv465@cam.ac.uk>
2020-10-05 22:16:50 +0100

    Add prediction for first order MM

--- src/markov_chains/markov_model.py
+++ src/markov_chains/markov_model.py
@@ -1,20 +1,41 @@
 import numpy as np
 import pandas as pd
+import random
 
 def generateFirstOrderMarkov(data):
     """
     Given a list of the past moves of RPS, generate a transition matrix for it.
 
-    Input: A list of strings of R, P, S, or O for other
+    Args:
+        data: A list of strings of R, P, S, or O for other (List[String])
 
-    Output: A dictionary to a dictionary to a probability
+    Returns:
+        A dictionary mapping the last move to a dictionary which maps the possible moves to their probabilities
+            (String -> (String -> float))
     """
     return pd.crosstab(pd.Series(data[1:],name='LastMove'),pd.Series(data[:-1],name='NextMove'),normalize=1).to_dict()
 
 def rpsMarkovModel(data):
+    """
+    Returns the proper markov model in case of future development with higher order Markov models
+
+    Args:
+        data: a list of the past moves that were done (List[String])
+
+    Returns:
+        A dictionary mapping the last move to a dictionary which maps the possible moves to their probabilities
+            (String -> (String -> float))
+    """
     return generateFirstOrderMarkov(data)
 
-def prediction(move, model):
-    return 0
+def prediction(lastMove, model):
+    """
+    Given the Markov model and the last move that was done, predicts the next move 
 
-print(rpsMarkovModel(["R", "P", "S", "P", "R", "P", "S", "R", "R", "P"]))
+    Args:
+        lastMove: A string which has what the last move played was. (String)
+        model: A dictionary mapping the last move to a dictionary which maps the possible moves to their probabilities
+            (String -> (String -> float))
+
+    """
+    return random.choices(list(model[lastMove].keys()), weights=model[lastMove].values())[0]
