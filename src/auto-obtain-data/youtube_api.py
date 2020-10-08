9783be7ac592dbad94d58a2befc5e90b0b95b921
Manu Varma <mv465@cam.ac.uk>
2020-10-08 17:29:58 +0100

    Add ability to get only the videos with わためのうたin the title

--- src/auto-obtain-data/youtube_api.py
+++ src/auto-obtain-data/youtube_api.py
@@ -16,13 +16,39 @@ youtube = googleapiclient.discovery.build(
 
 
 def getAllUploadsFromWatame():
+    """
+    Returns the videos that Watame Ch. has on her channel
+
+    Returns:
+        videos: A dictionary which has a list of all of the videos. The keys of this dictionary are ['kind', 'etag', 'nextPageToken', 'items', 'pageInfo']. 
+
+    """
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     # os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     request = youtube.playlistItems().list(
         part="snippet,contentDetails",
-        maxResults=25,
+        maxResults=50,
         playlistId=uploads_playlist
     )
     return request.execute()
+
+def getWatameNoUta(videos):
+    """
+    Given a response from the youtube API which is a list of videos, returns all of the videos that were Watame no Uta.
+
+    Keyword Arguments:
+        videos: A dictionary which has a list of all of the videos. The keys of this dictionary are ['kind', 'etag', 'nextPageToken', 'items', 'pageInfo']. 
+
+    Returns:
+        List of videos that had Watame no Uta in the title. The keys of this dictionary will be ['kind', 'etag', 'id', 'snippet', 'contentDetails']
+    """
+    
+    wataUtas = []
+
+    for video in videos['items']:
+        if "わためのうた" in video['snippet']['title']:
+            wataUtas.append(video)
+
+    return wataUtas
